038748dd8dec6f868079230c50fe792bb6c48af3
Jacopo Calvi <jacopo.bob.calvi@gmail.com>
2025-12-06 12:27:15 +0000

    removed log pushed by mistake

--- src/lib/ui/drawing-board/DrawingBoard.svelte
+++ src/lib/ui/drawing-board/DrawingBoard.svelte
@@ -76,7 +76,6 @@
 	function handlePointerUp(e: PointerEvent) {
 		// ignore non-left clicks (mouse) and secondary touch points (touch)
 		if (e.button !== 0 || !e.isPrimary) return;
-		console.log(e);
 		editor.stateManager.currentState?.handlePointerUp(getEventContext(e));
 	}
 
